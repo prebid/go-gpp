d029401c06fd6cebdaef26df889e83535d39ee8c
dudu <hongxingp@opera.com>
2023-02-17 17:50:26 +0800

    Add some comments.

--- util/encoding.go
+++ util/encoding.go
@@ -9,6 +9,7 @@ var (
 	fibEncodeNumOutOfRangeErr = fmt.Errorf("the number to be encoded is out of range")
 )
 
+// enlarge enlarges the underlying byte slice.
 func (bs *BitStream) enlarge(n uint16) {
 	final := (bs.p + n + 7) / 8
 	for final > bs.Len() {
@@ -16,6 +17,7 @@ func (bs *BitStream) enlarge(n uint16) {
 	}
 }
 
+// appendNBits appends n bits in b from left to right to the BitStream.
 func (bs *BitStream) appendNBits(b []byte, n uint16) {
 	bs.enlarge(n)
 	var (
@@ -31,55 +33,68 @@ func (bs *BitStream) appendNBits(b []byte, n uint16) {
 	}
 }
 
+// Base64Encode applies base64 encoding on the data in buffer.
 func (bs *BitStream) Base64Encode() []byte {
 	encoded := make([]byte, base64.RawURLEncoding.EncodedLen(len(bs.b)))
 	base64.RawURLEncoding.Encode(encoded, bs.b)
 	return encoded
 }
 
+// Reset clears all the data a BitStream holds.
 func (bs *BitStream) Reset() {
 	bs.b = nil
 	bs.p = 0
 }
 
+// WriteByte1 writes the rightmost bit in b into the BitStream.
 func (bs *BitStream) WriteByte1(b byte) {
 	bs.appendNBits([]byte{b << 7}, 1)
 }
 
+// WriteByte2 writes the rightmost 2 bits in b into the BitStream.
 func (bs *BitStream) WriteByte2(b byte) {
 	bs.appendNBits([]byte{b << 6}, 2)
 }
 
+// WriteByte4 writes the rightmost 4 bits in b into the BitStream.
 func (bs *BitStream) WriteByte4(b byte) {
 	bs.appendNBits([]byte{b << 4}, 4)
 }
 
+// WriteByte6 writes the rightmost 6 bits in b into the BitStream.
 func (bs *BitStream) WriteByte6(b byte) {
 	bs.appendNBits([]byte{b << 2}, 6)
 }
 
+// WriteByte8 .
 func (bs *BitStream) WriteByte8(b byte) {
 	bs.appendNBits([]byte{b}, 8)
 }
 
+// WriteUInt12 writes the rightmost 12 bits in b into the BitStream.
+// For instance, the input b is 0xff01, the effective part refers to the rightmost 12 bits,
+// which should be 0xf01.
 func (bs *BitStream) WriteUInt12(b uint16) {
 	first := byte(b >> 4)
 	second := byte(b << 4)
 	bs.appendNBits([]byte{first, second}, 12)
 }
 
+// WriteUInt16 .
 func (bs *BitStream) WriteUInt16(b uint16) {
 	first := byte(b >> 8)
 	second := byte(b)
 	bs.appendNBits([]byte{first, second}, 16)
 }
 
+// WriteTwoBitField .
 func (bs *BitStream) WriteTwoBitField(bList []byte) {
 	for _, b := range bList {
 		bs.WriteByte2(b)
 	}
 }
 
+// WriteFibonacciInt .
 func (bs *BitStream) WriteFibonacciInt(num uint16) error {
 	// The num should be [1,6765). Actually once the num is larger than or equal to 987,
 	// the efficiency of Fibonacci Encoding would be no better than 'WriteUint16'.
@@ -122,6 +137,7 @@ func (bs *BitStream) WriteFibonacciInt(num uint16) error {
 	return nil
 }
 
+// WriteIntRange .
 func (bs *BitStream) WriteIntRange(intRange *IntRange) error {
 	var err error
 	bs.WriteUInt12(intRange.Size)
